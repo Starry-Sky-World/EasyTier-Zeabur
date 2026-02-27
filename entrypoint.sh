a8741afd0472a146b283877f8d3230e66c7fda31
顾雨晨 <liu_qinyu@outlook.com>
2026-02-27 11:51:13 +0800

    Add logging for API host and server ports

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,5 @@
+echo "api host: ${API_HOST}"
+echo "server port: ${PORT}"
+echo "config server port: ${CONFIG_PORT}"
+
 ./easytier-web-embed --api-server-port ${PORT} --api-host ${API_HOST} --config-server-port ${CONFIG_PORT} --config-server-protocol tcp
